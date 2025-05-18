29f55fa9629dfc481de169b17e71be5dcdc7ec35
Love <love.tr2014@gmail.com>
2025-05-19 00:26:23 +0200

    title removed

--- src/App.jsx
+++ src/App.jsx
@@ -196,9 +196,6 @@ function App() {
           {activeView === 'canva' && (
             <>
               {/* This h1 was moved here from inside the card below */}
-              <h1 className="text-3xl sm:text-4xl font-bold text-teal-800 mb-6 sm:mb-8"> {/* text-emerald-800 to text-teal-800 */}
-                Léa and Love's Wedding!
-              </h1>
               <div className="bg-white bg-opacity-90 p-4 sm:p-6 md:p-8 rounded-2xl shadow-xl border border-gray-300 max-w-md w-full">
                 <div style={{ position: 'relative', width: '100%', height: 0, paddingTop: '177.7778%', paddingBottom: 0, boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)', marginTop: '1.6em', marginBottom: '0.9em', overflow: 'hidden', borderRadius: '8px', willChange: 'transform' }}>
                   <iframe loading="lazy" style={{ position: 'absolute', width: '100%', height: '100%', top: 0, left: 0, border: 'none', padding: 0, margin: 0 }}
